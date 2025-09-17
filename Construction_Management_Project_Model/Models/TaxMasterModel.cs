6425fe9696bf45211ee3e14048c9f1d969594857
tej1998-coder <tejalgira1998@gmail.com>
2025-09-17 09:48:08 +0530

    Add project files.

---
+++ Construction_Management_Project_Model/Models/TaxMasterModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construction_Management_Project_Model.Models
+{
+    internal class TaxMasterModel
+    {
+    }
+}
