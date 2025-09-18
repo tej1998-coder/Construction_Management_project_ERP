017e3667c2d78a84b4611effecb2c37a14638b63
tej1998-coder <tejalgira1998@gmail.com>
2025-09-18 10:29:20 +0530

    model push

---
+++ Construction_Management_Project_Model/Models/Bank_Model.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construction_Management_Project_Model.Models
+{
+     public class Bank_Model
+    {
+        [Key]
+        public int Bank_Id { get; set; }
+        public string? Bank_Name { get; set; }
+        public string? Remark { get; set; }
+        public int? Created_By { get; set; }
+        public int? Updated_By { get; set; }
+        public DateTime Created_Date { get; set; }
+        public DateTime Updated_Date { get; set; }
+        public int? Acflag { get; set; }
+        public string? Saas_Id { get; set; }
+ 
+    }
+}
+
+
---
+++ Construction_Management_Project_Model/Models/Roll_Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construction_Management_Project_Model.Models
+{
+    public class Roll_Model
+    {
+        [Key] 
+        public int Roll_Id { get; set; }
+        public string? Roll_Name { get; set; }
+        public string? Remark { get; set; }
+        public int? Created_By { get; set; }
+        public int? Updated_By { get; set; }
+        public DateTime Created_Date { get; set; }
+        public DateTime Updated_Date { get; set; }
+        public int? Acflag { get; set; }
+        public string? Saas_Id { get; set; }
+    }
+}
