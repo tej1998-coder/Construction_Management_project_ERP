1dd9c00fd0a2c5561869ab4cf8f2807ad6a0afd5
Harshita <harshitamali@110011>
2025-09-18 10:23:12 +0530

    changes

---
+++ Construction_ERP/Data/Context.cs
@@ -0,0 +1,13 @@
+﻿using Construction_Management_Project_Model.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Construction_ERP.Data
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            
+        }
+        public DbSet<EmployeeMaster_Model> Employee_Table { get; set; }
+    }
+}
---
+++ Construction_Management_Project_Model/Models/EmployeeMaster_Model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construction_Management_Project_Model.Models
+{
+    [Table("Employee_Table")]
+    public class EmployeeMaster_Model
+    {
+        [Key]
+        public int Emp_id { get; set; }
+        public string? Emp_Code { get; set; }
+        public string? Emp_Name { get; set; }
+        public string? Emp_Phone { get; set; }
+        public string? Emp_Pan { get; set; }
+        public string? Emp_Contact { get; set; }
+        public string? Emp_Email { get; set; }
+        public string? Emp_Bank_id { get; set; }
+        public string? Emp_Branch { get; set; }
+        public string? Emp_Account { get; set; }
+        public string? Emp_Address { get; set; }
+    }
+}
