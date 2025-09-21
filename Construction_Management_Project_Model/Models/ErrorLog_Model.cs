2fd61032857ff793114aa4fcd014b275e29c5b85
tej1998-coder <tejalgira1998@gmail.com>
2025-09-21 21:52:50 +0530

    test

---
+++ Construction_Management_Project_Model/Models/ErrorLog_Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construction_Management_Project_Model.Models
+{
+    public class ErrorLog_Model
+    {
+        [Key] 
+       public int ErrorLog_Id { get; set; }
+        [Required]
+       public System.DateTime ErrorLog_Date { get; set; }
+        [StringLength(200)]
+       public string?  controller_name  { get; set; }
+        [StringLength(200)]
+        public string? ActionMethodName { get; set; }
+        [Required]
+        public string? Data { get; set; }
+        public int? Flag { get; set; }
+    }
+}
