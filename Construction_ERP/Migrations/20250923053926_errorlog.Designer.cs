31d8ca405094fa558dfa0a39b39346bb02be5d1e
Sunil <Sunil@DESKTOP-DSUQ0CP>
2025-09-23 11:12:46 +0530

    save changes

--- Construction_ERP/Migrations/20250923053926_errorlog.Designer.cs
+++ Construction_ERP/Migrations/20250923053926_errorlog.Designer.cs
@@ -4,6 +4,7 @@ using Construction_ERP.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Construction_ERP.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250923053926_errorlog")]
+    partial class errorlog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
